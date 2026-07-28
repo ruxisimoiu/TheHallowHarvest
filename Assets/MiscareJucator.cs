fa974cf32c65590f75f16e399a7c62a06a4ffaf0
Ruxandra Simoiu <maria.simoiu@stud..acs.upb.ro>
2026-07-28 20:24:10 +0300

    proiect RGD

---
+++ Assets/MiscareJucator.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MiscareJucator : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("sa vedem daca merge");
+    }
+
+    void Update()
+    {
+        // Aici vom adăuga logica de mișcare mai târziu
+    }
+}
