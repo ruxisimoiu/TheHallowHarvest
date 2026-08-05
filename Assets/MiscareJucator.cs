8d27ee860b05b984e6a1fb92d38c0c001882b349
Ruxandra Simoiu <maria.simoiu@stud..acs.upb.ro>
2026-08-05 16:52:18 +0300

    miscari sus jos stanga dreapta

--- Assets/MiscareJucator.cs
+++ Assets/MiscareJucator.cs
@@ -2,14 +2,29 @@ using UnityEngine;
 
 public class MiscareJucator : MonoBehaviour
 {
+    public float viteza = 5f;
+    private Rigidbody2D rb;
+    private float miscareOrizontala;
+    private float miscareVerticala;
+
     void Start()
     {
-        Debug.Log("sa vedem daca merge");
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        // am pus branch urile
-        // Aici vom adăuga logica de mișcare mai târziu
+        // Preia comenzi de la tastatură pentru toate cele 4 direcții
+        miscareOrizontala = Input.GetAxisRaw("Horizontal");
+        miscareVerticala = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // Creează direcția finală de mișcare
+        Vector2 directie = new Vector2(miscareOrizontala, miscareVerticala);
+        
+        // normalizat (normalized) previne mișcarea mai rapidă pe diagonală
+        rb.linearVelocity = directie.normalized * viteza;
     }
 }
